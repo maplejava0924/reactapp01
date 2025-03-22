47431da20847ecf0b5b119ff0212e978f3777d35
maple <maplements@gmail.com>
2025-03-22 09:50:22 +0000

    Add LCEL-based LLM interaction

--- app/backend/main.py
+++ app/backend/main.py
@@ -2,28 +2,48 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import time
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
 
 app = FastAPI()
 
 # CORS フロントエンドとバックエンド間の通信を許可
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 class ChatRequest(BaseModel):
     message: str
+
 
 class ChatResponse(BaseModel):
     reply: str
 
+
 @app.post("/chat", response_model=ChatResponse)
 def chat(req: ChatRequest):
-    # テスト用に5秒待機
-    time.sleep(5)
-    # 仮の返信（のちにLangChain処理に置き換える）
-    return ChatResponse(reply=f"（FastAPIの返答）あなたはこう言いましたね：{req.message}")
+    input = req.message
+
+    prompt = PromptTemplate.from_template("""ユーザからの入力に対してドラえもんとして答えて。
+    ユーザからの入力：{input}
+    """)
+
+    model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+    chain = (
+        {
+            "input": RunnablePassthrough(),
+        }
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+
+    return ChatResponse(reply=chain.invoke(input))
